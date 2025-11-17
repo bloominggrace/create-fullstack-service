a20753b7314ab71ef64d90b8b1d2702d426a28a9
Yosep Park <op5321@bloominggrace.world>
2025-11-17 10:14:38 +0900

    Web 폴더에 DevContainer 환경 구성
    - .gitignore에 .pnpm-store 항목 추가

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
 
 const queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
 }
 
-export default App
+export default App;
