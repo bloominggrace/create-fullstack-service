7e6be4eab7a9f1f221e843f54f80ee2bb4536d16
Daemyung Jang <djang@bloominggrace.world>
2025-11-25 17:41:20 +0900

    Pino 설정 (#6)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule, Params } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { isProduction } from './app.utils';
+
+const loggerConfig: Params = {
+  pinoHttp: {
+    level: isProduction() ? 'info' : 'debug',
+    transport: isProduction()
+      ? undefined
+      : {
+          target: 'pino-pretty',
+          options: {
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        },
+  },
+};
 
 @Module({
-  imports: [],
+  imports: [LoggerModule.forRoot(loggerConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
