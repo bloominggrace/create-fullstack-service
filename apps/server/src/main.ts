6ea4108d04019bace634bc2acfac7cb3649820b2
Inho Jeong <ihjeong@bloominggrace.world>
2025-11-28 10:58:33 +0900

    서버 URL 출력 (#16)

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+  await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
+  logger.log(`서버가 ${await app.getUrl()}에서 실행 중입니다.`);
 }
 
 bootstrap().catch(() => process.exit(1));
