70b53106a349ba86a68ad969a20a3afe73f9bf21
Inho Jeong <ihjeong@bloominggrace.world>
2025-12-01 18:16:10 +0900

    MikroORM 설정 (#10)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/core';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { LoggerModule, Params } from 'nestjs-pino';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { isProduction } from './app.utils';
+import { mikroOrmConfig } from './mikro-orm.config';
 
 const loggerConfig: Params = {
   pinoHttp: {
@@ -14,14 +18,36 @@ const loggerConfig: Params = {
           options: {
             translateTime: 'SYS:standard',
             ignore: 'pid,hostname',
+            singleLine: true,
           },
         },
   },
 };
 
 @Module({
-  imports: [LoggerModule.forRoot(loggerConfig)],
+  imports: [
+    LoggerModule.forRoot(loggerConfig),
+    MikroOrmModule.forRoot(mikroOrmConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit(): Promise<void> {
+    if (!isProduction()) {
+      await this.updateDatabase();
+    }
+  }
+
+  private async updateDatabase() {
+    const generator = this.orm.getSchemaGenerator();
+    await generator.ensureDatabase();
+
+    if ((await generator.getUpdateSchemaSQL()).length !== 0) {
+      await generator.updateSchema();
+      Logger.log(`DB가 업데이트 되었습니다.`, AppModule.name);
+    }
+  }
+}
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const logger = app.get(Logger);
   app.useLogger(logger);
   await app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0');
-  logger.log(`서버가 ${await app.getUrl()}에서 실행 중입니다.`);
+  logger.log(`서버가 ${await app.getUrl()}에서 실행 중입니다.`, 'bootstrap');
 }
 
 bootstrap().catch(() => process.exit(1));
